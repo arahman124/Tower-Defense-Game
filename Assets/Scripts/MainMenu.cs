a5106ba83e272bff66dafbe9171fac0b81037269
arahman124 <86189902+arahman124@users.noreply.github.com>
2022-11-29 18:04:55 +0000

    Play button
    Used the scene manager to switch scene from main menu to sample scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayPressed()
     {
-        Debug.Log("I HIT THE PLAY BUTTON");
+        SceneManager.LoadScene(1);
     }
 
     public void ControlsPressed()
