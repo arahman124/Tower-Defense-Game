3c5a25d4e7f88d706637b7a7502e0657c3aa6b60
arahman124 <86189902+arahman124@users.noreply.github.com>
2022-11-30 15:43:07 +0000

    Added comments and changed font size
    Adjusted the button font size so they fit inside the boxes. Commented the main menu script.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,21 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //Creates the game object for the controls panel
     [SerializeField] private GameObject m_controlsPanel;
 
     public void PlayPressed()
     {
+        //Displays this comment in the console to confirm that the button has been pressed - testing
+        Debug.Log("I Hit the Play button");
+        //Changes the scene to scene 1 which is the playing game state
         SceneManager.LoadScene(1);
     }
 
     public void ControlsPressed()
     {
+        //Displays this comment in the console to confirm that the button has been pressed - testing
+        Debug.Log("I Hit the Controls button");
+        //Sets the control panel to the opposite state (active or inactive) in order to make it appear (active means it appears)
         m_controlsPanel.SetActive(!m_controlsPanel.activeSelf);
     }
 
     public void QuitPressed()
     {
+        //Displays this comment in the console to confirm that the button has been pressed -testing
         Debug.Log("I HIT THE QUIT BUTTON");
-
+        //Closes the application
+        Application.Quit();
     }
 }
