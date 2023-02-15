ff3ed11499a5a860907a8e70831ece7a97759e2f
arahman124 <86189902+arahman124@users.noreply.github.com>
2023-02-15 12:07:30 +0000

    Adding comments
    Documentation is fun :)

--- Assets/Scripts/Arrows.cs
+++ Assets/Scripts/Arrows.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Arrows : MonoBehaviour
 {
+    //Rigidbody variable declared for arrow
     Rigidbody2D rb;
-
     // Start is called before the first frame update
     void Start()
     {
+        //Accesses the rigidbody for the attached object (arrow)
         rb = GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
+        //Calculates angle for arrow
         float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        //Rotates the arrow such that it has a nice arrow arc
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,38 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Weapon : MonoBehaviour
 {
+    //Arrow prefab
     [SerializeField] public GameObject arrow;
+    //Amount of force applied to the arrow
     [SerializeField] public float launchForce;
+    //Place where arrow is shot from
     [SerializeField] public Transform shotPoint;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
+        //Position of crossbow and mouse
         Vector2 crossbowPosition = transform.position;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Direction of the arrow
         Vector2 direction = mousePosition - crossbowPosition;
+        //Sets default direction to looking right
         transform.right = direction;
-
+        //Fires the weapon when LMC
         if (Input.GetMouseButtonDown(0))
         {
             Fire();
         }
     }
-
     private void Fire()
     {
+        //Instantiates a new arrow
         GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+        //Gives the arrow a force
         newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
-
     }
 }
