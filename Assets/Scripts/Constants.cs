46c3058a8c63fb8c25a655cbc58101556a1d8022
arahman124 <86189902+arahman124@users.noreply.github.com>
2023-02-22 14:58:25 +0000

    Goblin animations in game
    Hooked up through code and set in the editor

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,6 +1,20 @@
 public static class Constants
 {
+    #region GAMEOBJECT_TAGS
     //Variable to hold tag for the ground
     public static string GROUND_TAG = "Ground";
+    public static string TOWER_TAG = "Tower";
+    #endregion
+
+    #region COLLISION_LAYERS
     public static string ENEMY_LAYER = "Enemy";
+    #endregion
+
+    #region ANIMATION_TRANSITION_NAMES
+    public static string MONSTER_WALK = "Walk";
+    public static string MONSTER_HIT = "Hurt";
+    public static string MONSTER_DEAD = "Dead";
+    public static string MONSTER_IDLE = "Idle";
+    public static string MONSTER_ATTACK = "Attack";
+    #endregion
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,7 +6,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[RequireComponent(typeof(UIDocument))]
+[RequireComponent(typeof(UIDocument), typeof(Animator))]
 public class Enemy : MonoBehaviour
 {
     //variable to hold speed of monsters
@@ -18,6 +19,9 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private Transform m_healthBarLocation;
 
+    [SerializeReference] private float m_cooldownDuration;
+    private float m_cooldownTimer;
+
     public float Health
     {
         get => m_currentHealth;
@@ -25,10 +29,13 @@ public class Enemy : MonoBehaviour
         {
             m_currentHealth = value;
 
-            if(m_currentHealth <= 0)
+            State = EnemyState.Hurt;
+
+            if (m_currentHealth <= 0)
             {
-                // Kill the enemy
-                Destroy(gameObject);
+                State = EnemyState.Dead;
+
+                Destroy(gameObject, 2.5f);
             }
 
             int currentlyVisibleHearts = m_hearts.Count(x => x.visible);
@@ -40,7 +47,7 @@ public class Enemy : MonoBehaviour
             if (heartThreshold >= percentageHealth)
             {
                 int amountOfHeartsToLose = currentlyVisibleHearts - (RoundUp((int)percentageHealth, 20) / 20);
-                for(int i = 0; i < amountOfHeartsToLose; i++)
+                for (int i = 0; i < amountOfHeartsToLose; i++)
                 {
                     LoseHeart();
                 }
@@ -48,13 +55,77 @@ public class Enemy : MonoBehaviour
         }
     }
 
+
     private VisualElement m_healthBar;
     private VisualElement[] m_hearts;
 
     private Camera m_mainCamera;
 
+    private Animator m_animator;
+
+    public enum EnemyState
+    {
+        Walking,
+        Idle,
+        Dead,
+        Attack,
+        Hurt
+    }
+
+    private EnemyState m_state;
+
+    private bool m_atTower = false;
+
+    // We're using a property for the State so that we are able to trigger events when we set the value
+    public EnemyState State
+    {
+        get => m_state;
+
+        set
+        {
+            ResetAnimationTriggers();
+            switch (value)
+            {
+                case EnemyState.Walking:
+                    m_animator.SetTrigger(Constants.MONSTER_WALK);
+                    break;
+                case EnemyState.Idle:
+                    m_animator.SetTrigger(Constants.MONSTER_IDLE);
+                    break;
+                case EnemyState.Dead:
+                    m_animator.SetTrigger(Constants.MONSTER_DEAD);
+                    break;
+                case EnemyState.Hurt:
+                    m_animator.SetTrigger(Constants.MONSTER_HIT);
+                    m_cooldownTimer = m_cooldownDuration;
+                    break;
+                case EnemyState.Attack:
+                    m_animator.SetTrigger(Constants.MONSTER_ATTACK);
+                    m_cooldownTimer = m_cooldownDuration;
+                    break;
+            }
+            m_state = value;
+        }
+    }
+
+    // TODO - Wrap every animator.SetTrigger in a new function that calls ResetAnimationTriggers before setting the trigger
+    private void ResetAnimationTriggers()
+    {
+        // Unity is stupid and gets confused if triggers get set at the same time... 
+        // We want to clear them every time we set one
+        foreach (AnimatorControllerParameter param in m_animator.parameters)
+        {
+            if (param.type == AnimatorControllerParameterType.Trigger)
+            {
+                m_animator.ResetTrigger(param.name);
+            }
+        }
+    }
+
     void Start()
     {
+        m_animator = GetComponent<Animator>();
+
         m_mainCamera = Camera.main;
         //Queries
         m_healthBar = GetComponent<UIDocument>().rootVisualElement.Q("Container");
@@ -63,13 +134,45 @@ public class Enemy : MonoBehaviour
         SetHealthBarPosition();
 
         m_currentHealth = m_maxHealth;
+
+        State = EnemyState.Walking;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Transforms/changes position of the enemy such that it moves towards the given position
-        transform.position = Vector2.MoveTowards(transform.position, m_target.position, m_speed * Time.deltaTime);
+        switch (State)
+        {
+            case EnemyState.Walking:
+                //Transforms/changes position of the enemy such that it moves towards the given position
+                transform.position = Vector2.MoveTowards(transform.position, m_target.position, m_speed * Time.deltaTime);
+                break;
+
+            case EnemyState.Idle:
+                // TODO: SET UP TIMER FOR ATTACKING THE TOWER
+                break;
+            case EnemyState.Hurt:
+                m_cooldownTimer -= Time.deltaTime;
+
+                if (m_cooldownTimer <= 0f)
+                {
+                    if (m_atTower)
+                    {
+                        State = EnemyState.Idle;
+                    }
+                    else
+                    {
+                        State = EnemyState.Walking;
+                    }
+                }
+                break;
+        }
+
+    }
+
+    public void OnHurtAnimationFinished()
+    {
+        m_animator.SetTrigger(Constants.MONSTER_IDLE);
     }
 
     private void LateUpdate()
@@ -97,6 +200,15 @@ public class Enemy : MonoBehaviour
         VisualElement nextHeart = m_hearts.Where(x => x.visible).LastOrDefault();
 
         nextHeart.style.visibility = Visibility.Hidden;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(Constants.TOWER_TAG))
+        {
+            State = EnemyState.Idle;
+            m_atTower = true;
+        }
     }
 
 
