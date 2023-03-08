2fc4c74ba67c663b4a2d1cefe0ab84f9c4de2f1f
arahman124 <86189902+arahman124@users.noreply.github.com>
2023-03-08 09:49:18 +0000

    Waves loading from CSV

--- Assets/Scripts/Waves/WaveManager.cs
+++ Assets/Scripts/Waves/WaveManager.cs
@@ -12,17 +12,25 @@ public class WaveManager : MonoBehaviour
     };
 
     [System.Serializable]
-    public class Wave
+    public struct Wave
     {
-        //Name of type of wave - boss or normal
-        public string waveName;
-        //position of spawned
-        public Transform enemy;
-        //shouldn't be necessary while using object pooling - number of monsters to be spawned
-        public int count;
-        //Might need to be moved to object pooling script - dictates rate that monsters spawn in
-        public float spawnRate;
+        public Wave(int numSkeleton, int numGoblin, int numBoss, float spawnDelay)
+        {
+            NumSkeleton = numSkeleton;
+            NumGoblin = numGoblin;
+            NumBoss = numBoss;
+            SpawnDelay = spawnDelay;
+        }
+
+        readonly int NumSkeleton;
+        readonly int NumGoblin;
+        readonly int NumBoss;
+        readonly float SpawnDelay;
     }
+
+    private int m_currentWave;
+
+    private List<Wave> m_waves;
 
     public Wave[] waves;
     private int nextWave = 0;
@@ -35,6 +43,7 @@ public class WaveManager : MonoBehaviour
     private void Start()
     {
         waveCountdown = timeBetweenWaves;
+        ReadWaveDataFromCSV();
     }
 
     private void Update()
@@ -72,6 +81,25 @@ public class WaveManager : MonoBehaviour
 
     }
 
+    private void ReadWaveDataFromCSV()
+    {
+        m_waves = new List<Wave>();
+
+        TextAsset movesFile = (TextAsset)Resources.Load("waves");
+        string[] linesFromFile = movesFile.text.Split('\n');
+
+        for (int i = 1; i < linesFromFile.Length; ++i)
+        {
+            // Split the line into an array based on the commas
+            string[] contents = linesFromFile[i].Split(',');
+
+            int numSkeletons = int.Parse(contents[1]);
+            int numGoblins = int.Parse(contents[2]);
+            int numBosses = int.Parse(contents[3]);
+            float spawnDelay = float.Parse(contents[4].TrimEnd('\r'));
 
 
+            m_waves.Add(new Wave(numSkeletons, numGoblins, numBosses, spawnDelay));
+        }
+    }
 }
