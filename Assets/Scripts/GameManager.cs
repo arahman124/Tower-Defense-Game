6f34d45a3d28e84e07785947e57e766b900089ea
arahman124 <86189902+arahman124@users.noreply.github.com>
2023-03-08 11:57:06 +0000

    Weapon upgrades and more wave spawner stuff
    Weapon upgrades for crossbow

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager m_instance;
-
+    //Reference to the player
     [SerializeField] private Player m_player;
+    //Variable to reference the high score text
     [SerializeField] private TextMeshProUGUI m_highScoreText;
 
+    //Reference to the game over screen
     [SerializeField] private GameObject m_gameOverHUD;
     [SerializeField] private GameObject m_gameHUD;
 
+    //Boolean variable as a flag for if new high score acheived
     private bool m_wasNewHighScore = false;
 
+    //List in inspector for the different upgrades of crossbow - look in stats for the upgrade features
+    [SerializeField] List<Stats> m_crossBowStats;
+    //Initial level of crossbow set to element 0 in the list
+    private int m_crossbowUpgradeLevel = -1;
+
+    //List in inspector for the different upgrades of rifle - look in stats for the upgrade features
+    [SerializeField] List<Stats> m_rifleStats;
+    private int m_rifleUpgradeLevel;
+    private bool m_isRifleUnlocked;
+
+    //Instance for the singleton pattern
+    private static GameManager m_instance;
+
+
+    [Header("Wave Manager and Wave UI")]
+    [SerializeField] private WaveManager m_waveManager;
+    [SerializeField] private TextMeshProUGUI m_waveCounter;
+    [SerializeField] private TextMeshProUGUI m_totalMonsters;
+    [SerializeField] private Button m_nextWaveButton;
+
+    //Singleton pattern
     public static GameManager GetInstance()
     {
-        if(m_instance == null)
+        if (m_instance == null)
         {
             m_instance = new GameManager();
         }
@@ -29,50 +53,74 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Initalise singleton pattern
         m_instance = this;
 
+        //Sets the high score on screen - initally as 00000
         if (PlayerPrefs.HasKey(Constants.PLAYER_HIGH_SCORE))
         {
             m_highScoreText.text = $"HiScore: {PlayerPrefs.GetInt(Constants.PLAYER_HIGH_SCORE):00000}";
         }
 
+        //Sets the game HUD (the above UI) as visible
         m_gameHUD.SetActive(true);
+        //Makes sure game over screen is invisible
         m_gameOverHUD.SetActive(false);
 
         Time.timeScale = 1f;
+
+        // TODO: REMOVE 
+        //m_player.Gold = 1000;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_waveManager.HasEverythingSpawned() && m_waveManager.GetMonsterCount() == 0)
+        {
+            // The wave has ended, show the UI
+            m_nextWaveButton.gameObject.SetActive(true);
+            m_totalMonsters.gameObject.SetActive(false);
+        }
+        else
+        {
+            m_totalMonsters.text = $"Monsters Remaining: {m_waveManager.GetMonsterCount()}";
+        }
     }
 
+    //Method for when the game ends
     public void GameOver()
     {
         Time.timeScale = 0f;
 
+        //Calls SaveHighScore method, passing in the current points of the user
         SaveHighScore(m_player.Points);
 
         Debug.Log("Game Over");
 
         // TODO: Tell the object pooler to tell the enemies to hide their hearts
 
-
+        //Sets the current game HUD as invisible
         m_gameHUD.SetActive(false);
+        //Makes game over screen visible
         m_gameOverHUD.SetActive(true);
     }
 
+    //Method for checking if the new current score is higher than the current high score
     private void SaveHighScore(int playerScore)
     {
         // Save the player's highscore
         if (PlayerPrefs.HasKey(Constants.PLAYER_HIGH_SCORE))
         {
+            //Variable to hold the current high score
             int previousHiScore = PlayerPrefs.GetInt(Constants.PLAYER_HIGH_SCORE);
 
-            if(previousHiScore < playerScore)
+            //Checks if the current score is higher than the current high score
+            if (previousHiScore < playerScore)
             {
+                //Replaces the high score on screen with the current score acheived by player
                 PlayerPrefs.SetInt(Constants.PLAYER_HIGH_SCORE, playerScore);
+                //Flag for changed high score
                 m_wasNewHighScore = true;
             }
         }
@@ -82,8 +130,40 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //Method for restarting the game
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //Method for upgrading the crossbow
+    public void OnUpgradeCrossbowPressed()
+    {
+        //Checks if the upgrades have been maxed out for the crossbow
+        if (m_crossbowUpgradeLevel - 1 > m_crossBowStats.Count)
+        {
+            // Set text to say "MAXIMUM UPGRADE"
+            return;
+        }
+
+        // If gold is greater than next upgrade
+        if (m_crossBowStats[m_crossbowUpgradeLevel + 1].m_goldCost <= m_player.Gold)
+        {
+            Stats stats = m_crossBowStats[++m_crossbowUpgradeLevel];
+
+            // ++ increments the variable by one 
+            m_player.UpgradeWeapon(stats);
+            //Takes away cost from the player gold variable
+            m_player.Gold -= stats.m_goldCost;
+            Debug.Log("Upgraded weapon!");
+        }
+    }
+
+    public void OnNextWavePressed()
+    {
+        m_waveManager.OnStartWave();
+        m_nextWaveButton.gameObject.SetActive(false);
+        m_totalMonsters.gameObject.SetActive(true);
+        m_waveCounter.text = $"Wave: {m_waveManager.GetWaveCount()}/10";
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,20 +6,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Variables for the current points and gold that the user possesses
     private int m_points;
     private int m_gold;
 
+    //Text variable of the gold and points on screen
     [SerializeField] private TextMeshProUGUI m_pointsText;
     [SerializeField] private TextMeshProUGUI m_goldText;
 
+    //Reference to the weapon being used by the player
     [SerializeField] private Weapon m_weapon;
 
+    //Automatic property - Used instead of a new classes and new attributes with get and set methods
+    // Holds the points of the player 
     public int Points 
     { 
         get => m_points; 
 
         set
         {
+            //Updates points on screen
             m_points = value;
 
             // Add leading zeroes https://stackoverflow.com/questions/4325267/c-sharp-convert-int-to-string-with-padding-zeros 
@@ -26,12 +33,14 @@ public class Player : MonoBehaviour
         }
     }
 
+    //Automatic property for gold
     public int Gold 
     { 
         get => m_gold; 
 
         set
         {
+            //Updates gold count on screen
             m_gold = value;
 
             m_goldText.text = $"x {m_gold}";
@@ -43,7 +52,10 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (m_weapon == null)
+        {
+            Debug.Log("AAAAAAAAAAAAH!");
+        }
     }
 
     // Update is called once per frame
@@ -56,13 +68,33 @@ public class Player : MonoBehaviour
         }
     }
 
+    //Method for adding points - amount passed in as parameter
     public void AddPoints(int pointsToAdd)
     {
         Points += pointsToAdd;
     }
 
+    //Method for adding gold - amount passed in as parameter
     public void AddGold(int goldToAward)
     {
         Gold += goldToAward;
+    }
+
+    //Method for upgrading the weapon
+    public void UpgradeWeapon(Stats stats)
+    {
+        //stats script is shown in inspector for the next level of the weapon
+        m_weapon.SetDamage(stats.m_damage);
+        m_weapon.SetLaunchForce(stats.m_projectileSpeed);
+        m_weapon.SetCooldown(stats.m_rateOfFire);
+
+        // A better wya to do it would be to have a readonly enum WeaponType, and then cast based on the WeaponType variable
+        try
+        {
+            ((Rifle)m_weapon).SetAmmo(stats.m_ammoAmount);
+        }catch(Exception e)
+        {
+            Debug.Log("This obviosuly isn't a rifle...");
+        }
     }
 }
